b32755ffd6f14e05aaabdf1037eb729ff64ab5ac
anand <happycpp23@gmail.com>
2022-09-11 18:10:33 +0530

    added second line

--- first.cpp
+++ first.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"this is first program... edit in program";
+    cout<<"this is first program... edit in program \n";
+    cout<<"adding second line in first program"
 }
